1aff9989c4d4b732a9ffb8f01d3c2cac5f690232
Duchi Fejokwu <notihamdutch@gmail.com>
2022-11-25 16:02:11 +0000

    error action ignored

--- deploy_script.ps1
+++ deploy_script.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'stop'
 $StackName = 'quest-app'
 $TemplateBody = Get-Content -Path testing.yml -Raw
 
-if ((Get-CFNStack -StackName $StackName)) {
+if ((Get-CFNStack -StackName $StackName -ErrorAction Ignore)) {
    Update-CFNStack -StackName $StackName -TemplateBody $TemplateBody
 }
 else {
